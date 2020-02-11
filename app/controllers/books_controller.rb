8f720248edbce00c9a3ba7d0927188fdb09ed5ea
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-02-11 17:08:44 +0900

    [Add]edit機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,12 +9,19 @@ class BooksController < ApplicationController
 	end
 
 	def edit
+		@book = Book.find(params[:id])
 
 	end
 
 	def create
 		@book = Book.new(book_params)
 		@book.save
+		redirect_to books_path
+	end
+
+	def update
+		book = Book.find(params[:id])
+		book.update(book_params)
 		redirect_to books_path
 	end
 
