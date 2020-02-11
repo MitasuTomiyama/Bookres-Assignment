7125f0104da4ed907e09e39605c34e6b55b77f9d
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-02-11 16:30:00 +0900

    [Add]showページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,25 @@
 class BooksController < ApplicationController
-  def index
-  end
+	def index
+		@book = Book.new
+		@books = Book.all
+	end
 
-  def show
-  end
+	def show
+		@book = Book.find(params[:id])
+	end
 
-  def new
-  	@book = Book.new
-  end
+	def edit
 
-  def edit
-  end
+	end
+
+	def create
+		@book = Book.new(book_params)
+		@book.save
+		redirect_to books_path
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
 end
