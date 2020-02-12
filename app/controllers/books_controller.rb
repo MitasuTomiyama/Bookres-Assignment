e4afb1ab1fa668b02eb73843b2250ab578845a83
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-02-12 18:11:05 +0900

    [Fix]CreateBook-index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,9 @@ class BooksController < ApplicationController
 	end
 
 	def create
-		@book = Book.new(book_params)
-		if @book.save
-			redirect_to book_path(@book)
+		book = Book.new(book_params)
+		if book.save
+			redirect_to book_path(book)
 			flash[:notice] = "Book was successfully created."
 		else
 			@book = book
